35e5ae38b7e4d537f61dbcc765ae880bd337303e
lsy <lssy32@naver.com>
2023-04-11 11:40:47 +0900

    Initial commit

---
+++ src/main/java/com/example/todaysmoon/TodaysMoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.todaysmoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysMoonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodaysMoonApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/todaysmoon/TodaysMoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todaysmoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysMoonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
